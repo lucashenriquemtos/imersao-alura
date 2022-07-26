2bbe7e21901b923bc2a6cd2c99284bf14e467518
Lucas Henrique de Oliveira Matos <lucashenrique.mtos@gmail.com>
2022-07-26 05:08:15 -0300

    Adicionando anotações do MongoDB

--- linguagens/src/main/java/br/com/lh/api/linguagens/Linguagem.java
+++ linguagens/src/main/java/br/com/lh/api/linguagens/Linguagem.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 
@@ -11,7 +13,11 @@ import java.io.Serializable;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(collection = "linguagens")
 public class Linguagem implements Serializable {
+
+    @Id
+    private String id;
 
     private String title;
     private String image;

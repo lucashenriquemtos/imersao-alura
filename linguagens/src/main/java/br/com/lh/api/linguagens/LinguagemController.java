f7a8dc0d3a0041bfe9ff3ba6be478e07da47ab4d
Lucas Henrique de Oliveira Matos <lucashenrique.mtos@gmail.com>
2022-07-26 05:09:25 -0300

    Adicionando imports

--- linguagens/src/main/java/br/com/lh/api/linguagens/LinguagemController.java
+++ linguagens/src/main/java/br/com/lh/api/linguagens/LinguagemController.java
@@ -1,6 +1,9 @@
 package br.com.lh.api.linguagens;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;

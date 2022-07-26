7a4240d0a2eeef0ea40b3674277d145d4a759a08
Lucas Henrique de Oliveira Matos <lucashenrique.mtos@gmail.com>
2022-07-26 02:18:05 -0300

    Initial Commit

---
+++ linguagens/src/main/java/br/com/lh/api/linguagens/LinguagensApplication.java
@@ -0,0 +1,12 @@
+package br.com.lh.api.linguagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class LinguagensApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(LinguagensApplication.class, args);
+    }
+
+}
